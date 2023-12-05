7ef98467ba0750ad42397ea657418e0b33affb53
Edmond <edpodlegaev@yandex.ru>
2023-12-05 10:27:30 +0300

    Create something features for button

--- SparrowCode_ThirdTask/ContentView.swift
+++ SparrowCode_ThirdTask/ContentView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isTapped: Bool = false
     var body: some View {
-        HStack(spacing: -10) {
-            Image(systemName: "play.fill")
-            Image(systemName: "play.fill")
+        Button {
+            isTapped.toggle()
+        } label: {
+            HStack(spacing: -10) {
+                Image(systemName: "play.fill")
+                Image(systemName: "play.fill")
+            }
+            .foregroundStyle(isTapped ? .blue : .red)
+            .font(.system(size: 54))
         }
-        .foregroundStyle(.blue)
-        .font(.system(size: 54))
     }
 }
 
