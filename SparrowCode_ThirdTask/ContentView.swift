55992cb447ad3ff8ba8502cde12ad57a08988b87
Edmond <edpodlegaev@yandex.ru>
2023-12-05 21:55:44 +0300

    Unrefactor. Fckng animations

--- SparrowCode_ThirdTask/ContentView.swift
+++ SparrowCode_ThirdTask/ContentView.swift
@@ -19,39 +19,37 @@ struct ContentView: View {
             }
         } label: {
             if isChange {
-                withAnimation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0)) {
-                    HStack(spacing: -10) {
-                        Image(systemName: "play.fill")
-                            .offset(x: isTapped ? -15 : 21)
-                            .scaleEffect(isTapped ? 0 : 1)
-                            .opacity(isTapped ? 0 : 1)
-                        Image(systemName: "play.fill")
-                            .offset(x: isTapped ? -15 : 18)
-                        Image(systemName: "play.fill")
-                            .offset(x: isTapped ? -18 : 17)
-                            .scaleEffect(isTapped ? 1 : 0)
-                            .opacity(isTapped ? 1 : 0)
-                    }
-                    .foregroundStyle(.blue)
-                    .font(.system(size: 54))
+                HStack(spacing: -10) {
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? -15 : 21)
+                        .scaleEffect(isTapped ? 0 : 1)
+                        .opacity(isTapped ? 0 : 1)
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? -15 : 18)
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? -18 : 17)
+                        .scaleEffect(isTapped ? 1 : 0)
+                        .opacity(isTapped ? 1 : 0)
                 }
+                .foregroundStyle(.blue)
+                .font(.system(size: 54))
+                .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
             } else {
-                withAnimation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0)) {
-                    HStack(spacing: -10) {
-                        Image(systemName: "play.fill")
-                            .offset(x: isTapped ? 21 : -15)
-                            .scaleEffect(isTapped ? 1 : 0)
-                            .opacity(isTapped ? 1 : 0)
-                        Image(systemName: "play.fill")
-                            .offset(x: isTapped ? 18 : -15)
-                        Image(systemName: "play.fill")
-                            .offset(x: isTapped ? 17 : -18)
-                            .scaleEffect(isTapped ? 0 : 1)
-                            .opacity(isTapped ? 0 : 1)
-                    }
-                    .foregroundStyle(.blue)
-                    .font(.system(size: 54))
+                HStack(spacing: -10) {
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? 21 : -15)
+                        .scaleEffect(isTapped ? 1 : 0)
+                        .opacity(isTapped ? 1 : 0)
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? 18 : -15)
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? 17 : -18)
+                        .scaleEffect(isTapped ? 0 : 1)
+                        .opacity(isTapped ? 0 : 1)
                 }
+                .foregroundStyle(.blue)
+                .font(.system(size: 54))
+                .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
             }
         }
     }
