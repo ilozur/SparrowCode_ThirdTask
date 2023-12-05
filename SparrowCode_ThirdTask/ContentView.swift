3e3df5f19ded4a3cc8852a607b512f0d1da003c3
Edmond <edpodlegaev@yandex.ru>
2023-12-05 13:37:00 +0300

    Add right animation with DispatchQueue

--- SparrowCode_ThirdTask/ContentView.swift
+++ SparrowCode_ThirdTask/ContentView.swift
@@ -9,26 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isTapped: Bool = false
+    @State private var isChange: Bool = false
+    
     var body: some View {
         Button {
             isTapped.toggle()
-        } label: {
-            HStack(spacing: -10) {
-                Image(systemName: "play.fill")
-                    .offset(x: isTapped ? 38 : 0)
-                    .scaleEffect(isTapped ? 1 : 0)
-                    .opacity(isTapped ? 1 : 0)
-                    .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
-                Image(systemName: "play.fill")
-                    .offset(x: isTapped ? 38 : 0)
-                Image(systemName: "play.fill")
-                    .offset(x: isTapped ? 10 : 0)
-                    .scaleEffect(isTapped ? 0 : 1)
-                    .opacity(isTapped ? 0 : 1)
-                    .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                isChange.toggle()
             }
-            .foregroundStyle(.blue)
-            .font(.system(size: 54))
+        } label: {
+            if isChange {
+                HStack(spacing: -10) {
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? -15 : 20)
+                        .scaleEffect(isTapped ? 0 : 1)
+                        .opacity(isTapped ? 0 : 1)
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? -15 : 20)
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? -20 : 15)
+                        .scaleEffect(isTapped ? 1 : 0)
+                        .opacity(isTapped ? 1 : 0)
+                }
+                .foregroundStyle(.blue)
+                .font(.system(size: 72))
+                .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
+            } else {
+                HStack(spacing: -10) {
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? 20 : -15)
+                        .scaleEffect(isTapped ? 1 : 0)
+                        .opacity(isTapped ? 1 : 0)
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? 20 : -15)
+                    Image(systemName: "play.fill")
+                        .offset(x: isTapped ? 15 : -20)
+                        .scaleEffect(isTapped ? 0 : 1)
+                        .opacity(isTapped ? 0 : 1)
+                }
+                .foregroundStyle(.blue)
+                .font(.system(size: 72))
+                .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
+            }
         }
     }
 }
