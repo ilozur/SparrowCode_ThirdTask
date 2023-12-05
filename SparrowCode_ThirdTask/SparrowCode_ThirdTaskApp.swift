11544bb3f230d2e53b189256bafd625c743a1eaf
Edmond <edpodlegaev@yandex.ru>
2023-12-05 10:03:57 +0300

    Initial Commit

---
+++ SparrowCode_ThirdTask/SparrowCode_ThirdTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparrowCode_ThirdTaskApp.swift
+//  SparrowCode_ThirdTask
+//
+//  Created by Edmond Podlegaev on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparrowCode_ThirdTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
